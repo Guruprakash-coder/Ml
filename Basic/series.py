e5693c43a0e5e1b3d87ac580b4ce8de8440276ed
Guruprakash-coder <guruprakash6999@gmail.com>
2026-04-03 15:18:36 +0530

    'update'

---
+++ Basic/series.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr  3 14:46:08 2026
+
+@author: gurup
+"""
+
+import pandas as pd
+s=pd.Series([10,20,30,40,50])
+
+print(s.dtype)
+
+s.name="nums"
+print(s.values)
+
+
+ 
